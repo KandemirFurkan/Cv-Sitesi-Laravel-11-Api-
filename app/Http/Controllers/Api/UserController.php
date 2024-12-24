e29799b811309b81654a7d0bad1baa699ae53c43
KandemirFurkan <furkankandemirr@gmail.com>
2024-12-24 20:54:23 +0300

    login register v2

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,45 +8,44 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
+
+
 class UserController extends Controller
 {
-
-public function register(Request $request){
-
-    $request->validate([
-'name'=>'required|string',
-'email'=>'required|email|unique:users,mail',
-'password'=>'required|string|min:6',
-]);
-
-$user=User::create([
-'name'=>$request->name,
-'email'=>$request->email,
-'password'=>Hash::make($request->password),
-]);
-return response()->json(['message'=>'Başarıyla Kayıt Olundu!',201]);
-}
-
-
-public function login(Request $request){
-
-    $request->validate([
-
-        'email'=>'required|email',
-        'password'=>'required|',
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
         ]);
-if(Auth::attempt($request->only('email','password'))){
 
-  $user=User::where('email',$request->email)->first();
-$token=$user->createToken('auth_token')->plaintextToken;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-return response()->json(['token'=>$token],200);
+        return response()->json(['message' => 'Başarıyla Kayıt Olundu.'], 201);
+    }
 
-}
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
 
-return response()->json(['message'=>'Unauthorized'],401);
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('auth_token')->plainTextToken;
 
-}
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
 
 
 
