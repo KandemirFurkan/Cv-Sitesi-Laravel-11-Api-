e9420b8af8ba73c03a3180cc736c460b44cccd96
KandemirFurkan <furkankandemirr@gmail.com>
2025-10-27 16:42:20 +0300

    Category & Skills

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SocialMedia;
+use App\Models\Tag;
+use App\Models\Blog;
+use App\Models\About;
 use App\Models\Slider;
+use App\Models\Project;
+use App\Models\Category;
+use App\Models\SiteSetting;
 use Illuminate\Http\Request;
-use App\Http\Controllers\PageController;
+
 
 class PageController extends Controller
 {
+    public function index(){
+         $slider = Slider::where('status',1)->first();
 
-   public function index(){
-     $slider= Slider::where('status',1)->first();
-     return view('frontend.index',compact('slider'));
+         $categories = Category::where('status',1)->get();
+         $about = About::first();
 
-   }
+         $skills = Tag::where('status',1)->paginate(6);
 
+         $projects = Project::where('status',1)->orderBy('id','desc')->limit(4)->get();
+
+         $blogs = Blog::where('status',1)->orderBy('id','desc')->limit(4)->get();
+
+        $setting = SiteSetting::pluck('setting_value','setting_key')->toArray();
+
+         // $socialmedias = SocialMedia::where('status',1)->orderBy('id','desc')->get();
+
+
+         return view('frontend.index',compact('slider','about','categories','skills','projects','blogs','setting'));
+    }
 }
